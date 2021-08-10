f304411bf462c798f2051156728d1177cedeacc3
wesleyynguyenn <52009450+wesleyynguyenn@users.noreply.github.com>
2021-08-09 20:30:18 -0700

    Add HTML templates and input validation
    Co-authored-by: Jonny Tran <61168867+jonathanktran@users.noreply.github.com>

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
 
 # A very simple Flask Hello World app for you to get started with...
-from main_flask import pixelator
+from main import pixelator
 from flask import Flask, request, render_template
+from markupsafe import Markup
 import os
 import base64
 import io
@@ -9,13 +10,12 @@ import io
 UPLOAD_FOLDER = '/home/wlnguyen/mysite/static/uploads'
 
 app = Flask(__name__)
-app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["DEBUG"] = True
 
 @app.route("/", methods=["GET", "POST"])
 def image_input():
-    if request.method == "POST":
+    if request.method == "POST" and request.form['pixel_size'].isnumeric() and not request.files['img'].filename == "":
         image = request.files['img']
         img_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
         image.save(img_path)
@@ -28,16 +28,15 @@ def image_input():
         encoded_img_data = base64.b64encode(data.getvalue())
         return render_template("display.html", img_data=encoded_img_data.decode('utf-8'))
 
-    return '''
-        <html>
-            <body>
-                <form method="post" action="." enctype="multipart/form-data">
-                    <p>Provide an image: <br>
-                    <input type="file" id='img' name='img'
-                    accept="image/png, image/jpeg, image/jpg"> </p>
-                    <p>Enter pixel size: <br> <input name="pixel_size" /></p>
-                    <p><input type="submit" value="Pixelate image" /></p>
-                </form>
-            </body>
-        </html>
-    '''
+    elif request.method == "POST" and not request.form['pixel_size'].isnumeric() and not request.files['img'].filename == "":
+        return render_template("homepage.html", image_error=Markup('<span id="error"> Please upload the image again! </span> <span class="brsmall"></span>'),
+            pixel_error=Markup('<span id="error"> Please enter an integer! </span><span class="brsmall"></span>'))
+
+    elif request.method == "POST" and request.form['pixel_size'].isnumeric() and request.files['img'].filename == "":
+        return render_template("homepage.html", image_error=Markup('<span id="error"> Please select an image! </span> <span class="brsmall"></span>'))
+
+    elif request.method == "POST":
+        return render_template("homepage.html", image_error=Markup('<span id="error"> Please select an image! </span> <span class="brsmall"></span>'),
+            pixel_error=Markup('<span id="error"> Please enter an integer! </span><span class="brsmall"></span>'))
+
+    return render_template("homepage.html")
